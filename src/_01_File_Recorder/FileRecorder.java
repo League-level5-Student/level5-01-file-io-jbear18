2bdba349ecde2a1a689f1bf0a63401cb3ece8dee
jbear18 <theyodelingcowgirl24@gmail.com>
2021-03-17 19:58:35 -0700

    decryptor ip

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,19 @@
 package _01_File_Recorder;
 
+import javax.swing.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+    public static void main(String[] args) {
+        try{
+         FileWriter file = new FileWriter("src/_01_File_Recorder/message");
+         String message= JOptionPane.showInputDialog("Write something!");
+         file.write("\n" + message);
+         file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,5 +1,10 @@
 package _02_File_Encrypt_Decrypt;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class FileDecryptor {
 	/*
 	 * Decryption is the process of taking encoded or encrypted text or other data
@@ -19,4 +24,36 @@ public class FileDecryptor {
 	 * Create a program that opens the file created by FileEncryptor and decrypts
 	 * the message, then display it to the user in a JOptionPane.
 	 */
+
+	char[] before= {'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D'};
+	char[] after= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	public static void main(String[] args) {
+		try{
+		BufferedReader br= new BufferedReader(new FileReader(src/_02_File_Encrypt_Decrypt/message));
+		String encrypted= br.readLine();
+		br.close();
+		char[] chars= new FileDecryptor().decrypt(encrypted);
+		String message= new String(chars);
+			JOptionPane.showMessageDialog(null, message);
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		} catch(IOException e) {
+			e.printStackTrace();
+
+	}
+
+	char[] decrypt(String input){
+		char[] chars= input.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = 0; j < before.length; j++) {
+				if(chars[i] == before[j]){
+					chars[i] = after[j];
+					break;
+				}
+			}
+		}
+		return chars;
+	}
 }
