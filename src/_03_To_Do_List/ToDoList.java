b23d2f0d649de00f691641b194a694d207345a44
jbear18 <theyodelingcowgirl24@gmail.com>
2021-04-14 20:03:26 -0700

    ToDoList IP

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,6 +1,25 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ToDoList implements ActionListener{
+	ArrayList<String> tasks= new ArrayList<String>();
+	FileWriter filewriter;
+	JFrame frame;
+	JPanel panel;
+	JButton addTask;
+	JButton viewTask;
+	JButton removeTask;
+	JButton saveList;
+	JButton loadList;
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	 *
@@ -21,4 +40,60 @@ public class ToDoList {
 	 * 
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
+	
+	public static void main(String[] args) {
+		new ToDoList().createFrame();
+	}
+
+	private void createFrame() {
+		frame= new JFrame();
+		panel= new JPanel();
+		addTask= new JButton("Add Task");
+		viewTask= new JButton("View Task");
+		removeTask= new JButton("Remove Task");
+		saveList= new JButton("Save List");
+		loadList= new JButton("Load List");
+		frame.add(panel);
+		panel.add(addTask);
+		panel.add(viewTask);
+		panel.add(removeTask);
+		panel.add(saveList);
+		panel.add(loadList);
+		addTask.addActionListener(this);
+		viewTask.addActionListener(this);
+		removeTask.addActionListener(this);
+		saveList.addActionListener(this);
+		loadList.addActionListener(this);
+		frame.setVisible(true);
+		frame.pack();
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	if(e.getSource()==(addTask)) {
+tasks.add(JOptionPane.showInputDialog("Enter a task:"));
+	}
+	else if(e.getSource()== (viewTask)) {
+		for(int i=0; i< tasks.size();i++) {	
+			System.out.println(tasks.get(i));
+		}
+	}
+	else if(e.getSource()== (removeTask))	{
+		String[] options = new String[tasks.size()];
+		for(int i=0; i< tasks.size(); i++) {
+			options[i] = tasks.get(i);
+		}
+		int remove = JOptionPane.showInputDialog("What task would you like to remove?","task removal", JOptionPane.DEFAULT_OPTION,  JOptionPane.INFORMATION_MESSAGE, null, options,options[options.length-1] );
+		tasks.remove(remove);
+	}
+	else if(e.getSource()== (saveList)) {
+		
+	}
+	else if(e.getSource()==(loadList)) {
+		
+	}
+	}
 }
